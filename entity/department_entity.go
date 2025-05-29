0de4171585f1b40dceef0ae31541e991e667da19
Justin Chow <justin.chow05@gmail.com>
2025-05-30 02:06:38 +0700

    fix: remove unique department name

--- entity/department_entity.go
+++ entity/department_entity.go
@@ -6,7 +6,7 @@ import (
 
 type Department struct {
 	ID      uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	Name    string    `gorm:"type:varchar(100);not null;unique" json:"name" validate:"required,min=2,max=100"`
+	Name    string    `gorm:"type:varchar(100);not null" json:"name" validate:"required,min=2,max=100"`
 	Faculty string    `gorm:"type:varchar(100);not null" json:"faculty" validate:"required,min=2,max=100"`
 
 	Timestamp
