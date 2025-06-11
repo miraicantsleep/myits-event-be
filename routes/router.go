5542a4a970a24f0100cd47de9932940c247add57
Mathias <mathiassuganda@gmail.com>
2025-06-11 22:35:14 +0700

    Revert "feat: Implement Booking Request feature and fix DI panic"
    This reverts commit f205ec3b2d609af6cf61d345027c8b0fc197846c.

--- routes/router.go
+++ routes/router.go
@@ -2,14 +2,15 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/miraicantsleep/myits-event-be/constants"
 	"github.com/miraicantsleep/myits-event-be/controller"
+	"github.com/miraicantsleep/myits-event-be/service"
 	"github.com/samber/do"
 )
 
 func BookingRequest(server *gin.Engine, injector *do.Injector) {
-	bookingRequestController := do.MustInvokeNamed[controller.BookingRequestController](injector, constants.BookingRequestController)
-	BookingRequestRoutes(server, bookingRequestController)
+	bookingRequestController := do.MustInvoke[controller.BookingRequestController](injector)
+	jwtService := do.MustInvoke[service.JWTService](injector)
+	BookingRequestRoutes(server, bookingRequestController, jwtService)
 }
 
 func RegisterRoutes(server *gin.Engine, injector *do.Injector) {
